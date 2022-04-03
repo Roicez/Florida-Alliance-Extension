4ffb39136be5492bd9297f8a820a36bb6b256690
Roicez <102883924+Roicez@users.noreply.github.com>
2022-04-03 15:28:44 -0400

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
-                i.src = "https://cdn.discordapp.com/attachments/849095785747841054/959970828689211402/dotted-place-template.png";
+                i.src = "https://cdn.discordapp.com/attachments/959963428506701834/960259196622876702/Florida_Down_Dot_Template.png";
                 i.onload = () => {
                     if (i.width === i.height) {
                         i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
